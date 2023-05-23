31d65996801b7fad6dd69a2a1001dc0b3313221b
HCH96 <sjrnfl0412@kookmin.ac.kr>
2023-05-23 14:53:20 +0900

    루프 구조 변경

--- WinAPI_MetalSlug3/main.cpp
+++ WinAPI_MetalSlug3/main.cpp
@@ -1,7 +1,4 @@
-﻿// WinAPI_MetalSlug3.cpp : 애플리케이션에 대한 진입점을 정의합니다.
-//
-
-#include "framework.h"
+﻿#include "framework.h"
 #include "WinAPI_MetalSlug3.h"
 
 #define MAX_LOADSTRING 100
@@ -42,13 +39,22 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     MSG msg;
 
-    // 기본 메시지 루프입니다:
-    while (GetMessage(&msg, nullptr, 0, 0))
+    while (true)
     {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
         {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
+            if (msg.message == WM_QUIT)
+                break;
+
+            if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+        else
+        {
+
         }
     }
 
